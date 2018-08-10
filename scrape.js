006ec6f7197a413b0c15622323b980c034f70ab4
techiadarsh <singhadarsh0205@gmail.com>
2018-08-11 00:30:03 +0530

    updated js file

--- scrape.js
+++ scrape.js
@@ -6,14 +6,20 @@ const headingLocation = '.list-view-books';
 os
 .get(website)
 .find(headingLocation)
-.set({'title': '.title',
-'language' : 'attributes.attributes-head',
-' disc' :'.cover-discount-tag' ,
-'price': '.price-attrib',
-'status': '.action' ,
- 'category': '.related-categories' ,
- 'AuthorName':'.author-publisher',
- 'amazon': '.action-btns'})
-
+.set(
+{
+'title': '.title',
+'disc' :'.cover-discount-tag' ,
+'cover'  : ' .cover a@href' ,
+'status': '.action .available-stock' ,
+'lang' : '.price-attrib .attributes tr[3] .attributes-title',
+'publisher' : ' .product-summary .author-publisher .a ',
+'category': '.related-categories a',
+'price': '.price-attrib .price .sell',
+'amazon' : ' .list-view-books .action .action-btns a:first@href' ,
+'pub-year' : ' .search-results .left-column .search-criteria ul[5] li[2] ',
+'author-name' : '.product-summary .author-publisher a'
+})
 .data(function(d) {
     console.log(d)})
+
